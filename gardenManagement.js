f8a0f9d2e61b590880068f8522661bef6f92d312
Samantha Bora <samantha.babubora@gmail.com>
2025-12-01 13:59:41 +0300

    complete phase 0 JS statement lab

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,25 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+// ----- Temperature Control -----
+if (temperature > 80) {
+  console.log("Watering on");
+} else {
+  console.log("Watering off");
+}
+
+// ----- Garden Lights Control -----
+if (timeOfDay === "evening" || timeOfDay === "night") {
+  console.log("Lights on");
+} else {
+  console.log("Lights off");
+}
+
+// ----- Soil Moisture Loop -----
+while (soilMoisture < 40) {
+  console.log(soilMoisture);
+  soilMoisture += 5;
+}
+
+console.log(40);
